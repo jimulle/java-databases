7ba16d7b979eb84d3e703a82c3a0d2a71026f10b
jim ulle <julle@gaig.com>
2017-03-06 10:23:03 -0500

    mySql database connection example

---
+++ src/db.java
@@ -0,0 +1,10 @@
+import database.DatabaseExample;
+
+public class db {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DatabaseExample.main(new String[0]);
+	}
+
+}
